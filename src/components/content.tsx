94ce3d41250bad45436aeebb14ee94395b90474a
Leonardo <leonardo.nasmt@gmail.com>
2026-01-21 22:40:02 -0300

    small fixes

--- src/components/content.tsx
+++ src/components/content.tsx
@@ -2,17 +2,22 @@ import Language from "./language-selector";
 import Projects from "./projects";
 
 export default function Content() {
+
+    const user = "leonardo.nasmt";
+    const at = "@";
+    const domain = "gmail.com";
+
     return (
         <div className="w-[50%] h-screen flex flex-col gap-y-15 py-25 px-30 text-neutral-200">
             <div className="flex flex-col gap-y-10">
                 <div className="flex justify-between items-center">
-                    <h1 className="font-semibold">Leonardo Nascimento</h1>
+                    <h1 className="font-semibold text-neutral-200">Leonardo Nascimento</h1>
                     <Language />
                 </div>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Non esse atque odit! Amet in quia porro, ratione cumque dolore officiis quasi quidem alias soluta modi, deleniti minus maxime repudiandae esse!</p>
             </div>
             <div className="flex flex-col gap-y-1">
-                <h3 className="mb-2 text-md text-zinc-400">Projects</h3>
+                <h3 className="mb-2 text-md text-neutral-400">Projects</h3>
                 <div className="flex gap-x-3 justify-between">
                     <Projects />
                     <Projects />
@@ -20,12 +25,12 @@ export default function Content() {
                 </div>
             </div>
             <div className="flex flex-col gap-y-1">
-                <h3 className="mb-2 text-md text-zinc-400">Now</h3>
+                <h3 className="mb-2 text-md text-neutral-400">Now</h3>
                 <p className="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas ipsum perferendis voluptatibus nobis, porro hic ab. Accusamus magni, ullam inventore recusandae sapiente ut maiores quod error molestias in nihil provident?</p>
             </div>
             <div className="flex flex-col gap-y-1">
-                <h3 className="mb-2 text-md text-zinc-400">Contact</h3>
-                <p className="">You can reach me at</p>
+                <h3 className="mb-2 text-md text-neutral-400">Contact</h3>
+                <p className="">You can reach me at <span className="underline decoration-neutral-400">{user}{at}{domain}</span>.</p>
             </div>
         </div>
     )
--- src/components/language-selector.tsx
+++ src/components/language-selector.tsx
@@ -2,9 +2,9 @@
 export default function Language() {
     return (
         <div className="flex gap-x-2 text-neutral-500">
-            <span className="hover:text-neutral-200">EN</span>
+            <span className="hover:text-neutral-200 hover:underline">EN</span>
             <span>/</span>
-            <span className="hover:text-neutral-200">PT</span>
+            <span className="hover:text-neutral-200 hover:underline">PT</span>
         </div>
     )
 }
