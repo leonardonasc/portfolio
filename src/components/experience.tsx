f83d3ab667806978052ca8cb15b01bd1b09a76f3
Leonardo <leonardo.nasmt@gmail.com>
2026-09-15 23:54:36 -0300

    cleaning up

--- src/components/experience.tsx
+++ src/components/experience.tsx
@@ -11,14 +11,14 @@ export default function Experience() {
             companyLink: "https://bluepaper.io",
             description: "Desenvolvimento de páginas web modernas e aplicações mobile, atuando em frontend e backend. Criação de interfaces responsivas, sistemas de gerenciamento e produtos digitais personalizados com Next.js, Tailwind CSS, AdonisJS, MySQL e Expo.",
         },
-        {
-            id: 2,
-            fromto: "Set 2026 — Atual",
-            role: "Desenvolvedor X Freelancer",
-            company: "X",
-            companyLink: "#",
-            description: "#",
-        }
+        // {
+        //     id: 2,
+        //     fromto: "Set 2026 — Atual",
+        //     role: "Desenvolvedor X Freelancer",
+        //     company: "X",
+        //     companyLink: "#",
+        //     description: "#",
+        // }
     ]
 
     return (
